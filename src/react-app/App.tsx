9e1f5e1614c7add17b1235f6ff0dbb42aae790c2
meas01 <measreaksmey01@gmail.com>
2025-11-26 12:45:14 +0700

    main

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
 
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
-import "./App.css";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
+// import honoLogo from "./assets/hono.svg";
+// import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
